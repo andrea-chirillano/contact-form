c09f02f8ee773639b3ab77243c27ebd6118b23e4
Andrea Chirillano <andreatchirillano@hotmail.com>
2023-12-30 14:53:16 -0300

    Fix: connection with database

--- process_form.php
+++ process_form.php
@@ -1,36 +1,30 @@
 <?php
-$connection = new mysqli("localhost", "id21722166_andrea", "Dogcat123!", "id21722166_contacts") or exit ('Could not connect to the database.');;
+$connection = new mysqli("localhost", "id21722166_andrea", "Dogcat123!", "id21722166_contacts") or exit ('Could not connect to the database.');
 
 if ($connection->connect_error) {
     die("Connection failed: " . $connection->connect_error);
 }
 
-// Get form 
-if (isset($_GET['submitted']) && $_GET['submitted'] === 'true' && $_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST['name'];
-    $last_name = $_POST['last_name'];
-    $phone_number = $_POST['phone_number'];
-    $email = $_POST['email'];
-    $content = $_POST['content'];
 
-    // Validate content length
-    if (strlen($content) < 10) {
-        die("The content must be at least 10 characters.");
-    }
-
-    // Insert data into the database
-    $sql = "INSERT INTO contacts (id, name, last_name, phone_number, email, content) 
-        VALUES (DEFAULT, '$name', '$last_name', '$phone_number', '$email', '$content')";
-
-    if ($connection->query($sql) === TRUE) {
-        echo "¡Successful registration!";
-    } else {
-        echo "Error: " . $sql . "<br>" . $connection->error;
-    }
-
-    $connection->close();
-} else {
-    echo "Connection error.";
+$name = $_POST['name'];
+$last_name = $_POST['last_name'];
+$phone_number = $_POST['phone_number'];
+$email = $_POST['email'];
+$content = $_POST['content'];
+// Validate content length
+if (strlen($content) < 10) {
+    die("The content must be at least 10 characters.");
 }
+// Insert data into the database
+$sql = "INSERT INTO contacts (id, name, last_name, phone_number, email, content) 
+    VALUES (DEFAULT, '$name', '$last_name', '$phone_number', '$email', '$content')";
+if ($connection->query($sql) === TRUE) {
+    echo "¡Successful registration!";
+} else {
+    echo "Error: " . $sql . "<br>" . $connection->error;
+}
+$connection->close();
 
 
+
+?>
